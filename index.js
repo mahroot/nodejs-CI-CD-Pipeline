c414ec93a548b8531863ffe475959f2ccdc71393
Gopendra Singh <146374805+mahroot@users.noreply.github.com>
2025-09-23 19:14:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('🚀 Hello World! This app is running via Docker + Jenkins CI/CD  This Task for day 2');
+  res.send('🚀 Hello World! This app is running via Docker + Jenkins CI/CD  This Task for day 2 ');
 });
 
 app.listen(PORT, () => {
